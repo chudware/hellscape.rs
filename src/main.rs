f3ff73db2b1c875b607e4a079c624abb7e02944a
John Smith <187957415+chudware@users.noreply.github.com>
2024-11-11 18:59:53 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+    let z_________e__r_________o = 0;
+    let o______n______e = z_________e__r_________o + 1;
+    let tw_____o = o______n______e + 1;
+    let tr_____ee = tw_____o + 1;
+    let f_____ou_____r = tr_____ee + 1;
+    let f_____ive = f_____ou_____r + 1;
+    let s_____ix = f_____ive + 1;
+    let sev_____en = s_____ix + 1;
+    let eig_____ht = sev_____en + 1;
+    let ni_____ne = eig_____ht + 1;
+    let t_____en = ni_____ne + 1;
+
+    println!("Numbers: {
+    } {
+    } {
+    } {
+    } {
+    } {
+    } {
+    } {
+    } {
+    } {
+    } {
+    }", 
+    z_________e__r_________o, 
+    o______n______e, 
+    tw_____o, 
+    tr_____ee, 
+    f_____ou_____r, 
+    f_____ive, 
+    s_____ix, 
+    sev_____en, 
+    eig_____ht, 
+    ni_____ne, 
+    t_____en);
+}
